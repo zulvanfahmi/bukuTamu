3bc8b2471f1b1fd0a880af76254bd022a3a557b7
zulvanF <zulvanfahmi1612@gmail.com>
2022-04-17 10:40:14 +0700

    menambahkan seeder utk 1 useradmin dan 5 bukutamu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Bukutamu;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // User::create([
+        //     'name' => 'Harry Maguire',
+        //     'email' => 'harrymaguire555@gmail.com',
+        //     'password' => bcrypt('qwerty1234?'),
+        //     'is_admin' => True,
+        // ]);
+
+        Bukutamu::factory(5)->create();
     }
 }
