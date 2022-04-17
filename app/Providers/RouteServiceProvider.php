77fa28ea49b168e941d7e85dd97bb3dafa9fc015
zulvanF <zulvanfahmi1612@gmail.com>
2022-04-17 13:01:39 +0700

    ubah default routes redirect dari home ke fobidden

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/forbidden';
+    // public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
